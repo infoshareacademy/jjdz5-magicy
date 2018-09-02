c634738d067f2d0b41adf74ab89381a180e822ea
KrzysztofG90 <krzysztofgotowala90@gmail.com>
2018-09-02 13:19:37 +0200

    first home-page template

--- web-app/src/main/java/com/infoshareacademy/usersengine/servlets/HelloServlet.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/servlets/HelloServlet.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy.usersengine.servlets;
 
 import com.infoshareacademy.usersengine.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -10,8 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet("/hello-servlet")
+@WebServlet("/home")
 public class HelloServlet extends HttpServlet {
 
     @Inject
@@ -19,13 +23,20 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter writer = resp.getWriter();
-
-        // writer.println("<!DOCTYPE html>");
-        writer.println("Hello from Servlet!");
 
 
+        Map<String, Object> dataModel = new HashMap<>();
+
+        Template template = templateProvider.getTemplate(getServletContext(), "home");
+
+        try{
+
+            template.process(dataModel, resp.getWriter());
+
+        }catch (TemplateException e){
+
+            e.printStackTrace();
+        }
 
 
 
