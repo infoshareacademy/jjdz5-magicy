11a63765f607ff526b05688839c890e60b661f04
KrzysztofG90 <krzysztofgotowala90@gmail.com>
2018-09-02 12:05:26 +0200

    freemarker added

--- web-app/src/main/java/com/infoshareacademy/usersengine/freemarker/TemplateProvider.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/freemarker/TemplateProvider.java
@@ -1,4 +1,34 @@
 package com.infoshareacademy.usersengine.freemarker;
 
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.servlet.ServletContext;
+import java.io.IOException;
+
+@ApplicationScoped
 public class TemplateProvider {
+
+    private static final String TEMPLATE_DIRECTORY_PATH = "WEB-INF/fm-templates";
+    private static final String TEMPLATE_EXTENSION = ".ftlh";
+
+
+    public Template getTemplate(ServletContext servletContext, String templateName) throws IOException{
+
+        final Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(true);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setServletContextForTemplateLoading(servletContext, TEMPLATE_DIRECTORY_PATH);
+
+        return cfg.getTemplate(templateName + ".ftlh");
+
+
+    }
+
+
 }
--- web-app/src/main/java/com/infoshareacademy/usersengine/servlets/HelloServlet.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/servlets/HelloServlet.java
@@ -1,5 +1,8 @@
 package com.infoshareacademy.usersengine.servlets;
 
+import com.infoshareacademy.usersengine.freemarker.TemplateProvider;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +13,10 @@ import java.io.PrintWriter;
 
 @WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
+
+    @Inject
+    private TemplateProvider templateProvider;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
@@ -17,5 +24,11 @@ public class HelloServlet extends HttpServlet {
 
         // writer.println("<!DOCTYPE html>");
         writer.println("Hello from Servlet!");
+
+
+
+
+
+
     }
 }
