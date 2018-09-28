bdce89b2aac33dd3136a6f93092ff9fef76cea5f
MeryWich <39101421+MeryWich@users.noreply.github.com>
2018-09-28 16:51:58 +0200

    Update AdvertsList.java

--- console-app/src/main/java/com/infoshareacademy/AdvertsList.java
+++ console-app/src/main/java/com/infoshareacademy/AdvertsList.java
@@ -7,9 +7,7 @@ public class AdvertsList {
     private final JsonToList jsonToList = new JsonToList();
     private final ReadProperties readProperties = new ReadProperties();
     private String path = readProperties.readFilePath();
-  //  private List<Advert> advertsList = jsonToList.jsonToList(path);
-    private List<Advert> advertsList = new ArrayList<>();
-
+    private List<Advert> advertsList = jsonToList.jsonToList(path);
 
     public void setAdvertsList(List<Advert> advertsList) {
         this.advertsList = advertsList;
