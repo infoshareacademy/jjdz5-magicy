3f797baf5637467aac2d52dcc38cd35233421745
krzysztof <krzysztofgotowala90@gmail.com>
2018-10-03 17:43:30 +0200

    add DriverManager interface

--- web-app/src/main/java/com/infoshareacademy/usersengine/driver/DriverManagerBean.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/driver/DriverManagerBean.java
@@ -1,24 +1,47 @@
 package com.infoshareacademy.usersengine.driver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infoshareacademy.Driver;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class DriverManagerBean implements DriverManager {
     @Override
     public List<Driver> addDriver(Driver driver, List<Driver> drivers) {
         drivers.add(driver);
-        return null;
+        return drivers;
     }
 
     @Override
     public Integer getNextDriverId(List<Driver> drivers) {
+        Integer idMax = 0;
+        for (Driver driver: drivers
+             ) {
+            if (driver.getId() > idMax){
+                idMax = driver.getId();
+            }
 
-        return null;
+        }
+
+        return idMax+1;
     }
 
     @Override
     public void driverToJson(List<Driver> driverList, String path) {
+        ObjectMapper objectMapper;
+        objectMapper = new ObjectMapper();
+        try {
+            String arrayToJson = objectMapper.writeValueAsString(driverList);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+            writer.write(arrayToJson);
+            writer.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
