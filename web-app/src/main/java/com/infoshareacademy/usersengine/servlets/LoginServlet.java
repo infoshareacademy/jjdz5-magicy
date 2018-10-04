accaac589fc728bb0351b86c8dc0c7673e3e7169
Artur Moroz <arturmoroz98@gmail.com>
2018-10-04 22:06:19 +0200

    Social media --> google+

--- web-app/src/main/java/com/infoshareacademy/usersengine/servlets/LoginServlet.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/servlets/LoginServlet.java
@@ -1,40 +1,123 @@
 package com.infoshareacademy.usersengine.servlets;
 
 
+
+
+
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+
+import com.infoshareacademy.usersengine.freemarker.TemplateProvider;
+
+import freemarker.template.Template;
+
+import freemarker.template.TemplateException;
+
+
+
+import javax.inject.Inject;
+
 import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
+
 import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
-    @WebServlet(urlPatterns = {"/login"})
-    public class LoginServlet extends HttpServlet {
-        @Override
-        protected void doPost (HttpServletRequest req,
-                               HttpServletResponse resp)
-                throws ServletException, IOException {
+import java.util.HashMap;
 
-            resp.setContentType("text/html");
+import java.util.Map;
 
-            try {
-                String idToken = req.getParameter("id_token");
-                GoogleIdToken.Payload payLoad = IdTokenVerifierAndParser.getPayload(idToken);
-                String name = (String) payLoad.get("name");
-                String email = payLoad.getEmail();
-                System.out.println("User name: " + name);
-                System.out.println("User email: " + email);
 
-                HttpSession session = req.getSession(true);
-                session.setAttribute("userName", name);
-                req.getServletContext()
-                        .getRequestDispatcher("/welcome-page.jsp").forward(req, resp);
 
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+@WebServlet(urlPatterns = {"/login"})
+
+public class LoginServlet extends HttpServlet {
+
+
+
+    @Inject
+
+    private TemplateProvider templateProvider;
+
+
+
+
+
+    @Override
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Map<String, Object> dataModel = new HashMap<>();
+
+        Template template = templateProvider.getTemplate(getServletContext(), "login");
+
+        try{
+
+            template.process(dataModel, resp.getWriter());
+
+        }catch (TemplateException e){
+
+            e.printStackTrace();
+
         }
+
     }
 
+
+
+    @Override
+
+    protected void doPost (HttpServletRequest req,
+
+                           HttpServletResponse resp)
+
+            throws ServletException, IOException {
+
+
+
+        resp.setContentType("text/html");
+
+
+
+        try {
+
+            String idToken = req.getParameter("id_token");
+
+            GoogleIdToken.Payload payLoad = IdTokenVerifierAndParser.getPayload(idToken);
+
+            String name = (String) payLoad.get("name");
+
+            String email = payLoad.getEmail();
+
+            System.out.println("User name: " + name);
+
+            System.out.println("User email: " + email);
+
+
+
+            HttpSession session = req.getSession(true);
+
+            session.setAttribute("userName", name);
+
+            req.getServletContext()
+
+                    .getRequestDispatcher("/welcome-page.jsp").forward(req, resp);
+
+
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+}
