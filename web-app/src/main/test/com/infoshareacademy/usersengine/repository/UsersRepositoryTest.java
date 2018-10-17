b1b1a168d43451c064f93f9b4c4d66f094251e38
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-10-17 14:33:39 +0200

    Created "UsersRepositoryTest" class.

---
+++ web-app/src/main/test/com/infoshareacademy/usersengine/repository/UsersRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.infoshareacademy.usersengine.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("UserRepository Test")
+class UsersRepositoryTest {
+
+    private UsersRepository usersRepository;
+
+    @BeforeEach
+    void setUp() {
+        usersRepository = new UsersRepository();
+    }
+
+//    @Test
+//    @DisplayName("")
+//    void
+
+}
