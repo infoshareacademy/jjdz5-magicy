bca9b9e157c45b89152d6504fbfde66349969f5a
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-10-19 15:21:57 +0200

    Added test for "findUserByUsernameAndPassword" method in "UsersRepositoryTest" class.

--- web-app/src/main/test/com/infoshareacademy/usersengine/repository/UsersRepositoryTest.java
+++ web-app/src/main/test/com/infoshareacademy/usersengine/repository/UsersRepositoryTest.java
@@ -1,23 +1,34 @@
 package com.infoshareacademy.usersengine.repository;
 
+import com.infoshareacademy.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("UserRepository Test")
 class UsersRepositoryTest {
+
+    private static final String TEST_NAME = "admin";
+    private static final String TEST_PASSWORD = "p";
 
     private UsersRepository usersRepository;
 
     @BeforeEach
     void setUp() {
         usersRepository = new UsersRepository();
+        usersRepository.init();
     }
 
-//    @Test
-//    @DisplayName("")
-//    void
+    @Test
+    @DisplayName("Should return appropriate User by given name and password.")
+    void returnsAppropriateUserByGivenNameAndPassword() {
+        // act
+        User userFromRepository = usersRepository.findUserByUsernameAndPassword(TEST_NAME, TEST_PASSWORD).get();
+        // assert
+        assertThat(userFromRepository.getName()).isEqualTo(TEST_NAME);
+        assertThat(userFromRepository.getPassword()).isEqualTo(TEST_PASSWORD);
+    }
 
 }
