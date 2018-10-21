8f58053782bc845b1349091d1ab85b374d242b0a
Grégòr Rùchniewicz <gregor.redar@gmail.com>
2018-10-21 15:42:07 +0200

    New commit, empty files created

---
+++ console-app/src/test/java/com/infoshareacademy/AdvertManagerTest.java
@@ -0,0 +1,32 @@
+package com.infoshareacademy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvertManagerTest {
+
+    @Test
+    void showAdverts() {
+    }
+
+    @Test
+    void showOneAdvert() {
+    }
+
+    @Test
+    void addAdvert() {
+    }
+
+    @Test
+    void writeAdvertData() {
+    }
+
+    @Test
+    void getMaxIdList() {
+    }
+
+    @Test
+    void promoteAdvert() {
+    }
+}
---
+++ console-app/src/test/java/com/infoshareacademy/DriverManagerTest.java
@@ -0,0 +1,24 @@
+package com.infoshareacademy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverManagerTest {
+
+    @Test
+    void showDrivers() {
+    }
+
+    @Test
+    void getMaxId() {
+    }
+
+    @Test
+    void giveRating() {
+    }
+
+    @Test
+    void writeDriverData() {
+    }
+}
