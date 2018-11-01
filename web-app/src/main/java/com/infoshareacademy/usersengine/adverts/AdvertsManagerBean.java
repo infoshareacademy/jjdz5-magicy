a9d713999699f2e53e74d061fb2995421ffd639f
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-11-01 20:38:53 +0100

    Added loggers and done some code refactor in AdvertsManagerBean class.

--- web-app/src/main/java/com/infoshareacademy/usersengine/adverts/AdvertsManagerBean.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/adverts/AdvertsManagerBean.java
@@ -2,13 +2,20 @@ package com.infoshareacademy.usersengine.adverts;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infoshareacademy.Advert;
-import javax.ejb.Stateful;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ejb.Stateless;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @Stateless
 public class AdvertsManagerBean implements AdvertsManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AdvertsManagerBean.class);
+    private final Integer valueToAdd = 1;
 
     public List<Advert> addAdvert(Advert advert, List<Advert> adverts){
         adverts.add(advert);
@@ -16,23 +23,16 @@ public class AdvertsManagerBean implements AdvertsManager {
     }
 
     public Integer getNextAdvertId(List<Advert> adverts){
-        Integer idMax = 0;
-        for(Advert advert: adverts){
-            if (advert.getId() > idMax) {
-                idMax = advert.getId();
-            }
-        }
-        return idMax+1;
+        Integer nextAdvertId = adverts.stream().mapToInt(Advert::getId).max().getAsInt() + valueToAdd;
+        LOG.debug("Next advert id value: " + nextAdvertId + ".");
+        return nextAdvertId;
     }
 
     public Integer getNextRouteId(List<Advert> adverts){
-        Integer idMax = 0;
-        for(Advert advert: adverts){
-            if (advert.getRoute().getId() > idMax) {
-                idMax = advert.getRoute().getId();
-            }
-        }
-        return idMax+1;
+        Integer nextRouteId = adverts.stream().mapToInt(advert -> advert.getRoute().getId()).max()
+                .getAsInt() + valueToAdd;
+        LOG.debug("Next route id value: " + nextRouteId + ".");
+        return nextRouteId;
     }
 
     public void advertsToJson(List<Advert> advertList, String path){
@@ -42,10 +42,10 @@ public class AdvertsManagerBean implements AdvertsManager {
             BufferedWriter writer = new BufferedWriter(new FileWriter(path));
             writer.write(arrayToJson);
             writer.close();
-
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.warn("IOException in advertsToJson method.");
         }
     }
+
 }
 
