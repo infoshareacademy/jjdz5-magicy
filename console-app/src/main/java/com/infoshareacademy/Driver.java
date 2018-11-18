63c0f3b2d48e613a4472ace8739ca0b7d299fce5
Maria Wicherkiewicz <maria.wicherkiewicz@gmail.com>
2018-11-18 12:11:27 +0100

    Changes in Driver

--- console-app/src/main/java/com/infoshareacademy/Driver.java
+++ console-app/src/main/java/com/infoshareacademy/Driver.java
@@ -1,6 +1,13 @@
 package com.infoshareacademy;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
