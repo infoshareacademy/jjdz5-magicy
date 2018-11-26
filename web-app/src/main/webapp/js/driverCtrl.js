14dbae9be2ca16cc95700d693e31f786b7950ee8
Maria Wicherkiewicz <maria.wicherkiewicz@gmail.com>
2018-11-26 23:02:15 +0100

    Ajax in drivers package

---
+++ web-app/src/main/webapp/js/driverCtrl.js
@@ -0,0 +1,59 @@
+function addDriver(){
+
+    $.ajax({
+        url : 'addDriverAjax',
+        data : {
+            name : $('#name').val(),
+            surname : $('#surname').val(),
+            city : $('#city').val(),
+            district : $('#district').val(),
+            phone : $('#phone').val()
+        },
+        success : function(responseText) {
+            console.log("response "+responseText);
+            console.log(responseText.length);
+            if (responseText.length == 3) {
+                $.ajax({
+                    type: 'POST',
+                    url: "add-driver",
+                    data: {
+                        name : $('#name').val(),
+                        surname : $('#surname').val(),
+                        city : $('#city').val(),
+                        district : $('#district').val(),
+                        phone : $('#phone').val()
+                    },
+                    dataType: "text",
+                    success: function (resultData) {
+                            swal('Great job!', 'New driver has been added', 'success');
+                            $('#name').val('');
+                            $('#surname').val('');
+                            $('#city').val('');
+                            $('#district').val('');
+                            $('#phone').val('');
+
+
+                    },
+                    error: function (resultData) {
+                        console.log(resultData);
+                    }
+            });
+            }
+            else{
+                swal({
+                        title: '<small style="width: 1200px;">This driver can not be added!</small>',
+                        text: '<label class="title">'+responseText+'</label>',
+                        confirmButtonText: "OK",
+                        html: responseText
+                    },
+                    function(isConfirm) {
+
+                    });
+            }
+        },
+        error: function (responseText) {
+            console.log(responseText);
+        }
+    });
+
+}
