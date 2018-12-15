b61e07224866253bd77d11cf57ad14c299ba1aa8
Maria Wicherkiewicz <maria.wicherkiewicz@gmail.com>
2018-12-15 16:58:24 +0100

    Fixed SecurityFilter

--- web-app/src/main/java/com/infoshareacademy/usersengine/filters/SecurityFilter.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/filters/SecurityFilter.java
@@ -1,8 +1,5 @@
 package com.infoshareacademy.usersengine.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -10,31 +7,17 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-//FIXME
-@WebFilter(urlPatterns = "/side")
+
+@WebFilter(urlPatterns = {"/home", "/adverts", "/add-advert", "/admin-panel", "/adverts-admin", "/about-us", "/maps-adverts", "/add-maps-advert"})
 public class SecurityFilter extends HttpFilter {
-
-    private static final String LOGIN_SERVLET_URL = "/jjdz5-magicy/login";
-    private static final String USER_NAME_ATTRIBUTE = "userName";
-
-    private Logger LOG = LoggerFactory.getLogger(SecurityFilter.class);
-
     @Override
-    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
-        if(req.getRequestURL().toString().endsWith(LOGIN_SERVLET_URL)) {
-            System.out.println(req.getRequestURL());
-            chain.doFilter(req, res);
-        } else {
-            Object logged = req.getSession().getAttribute(USER_NAME_ATTRIBUTE);
-            if(logged != null) {
-                LOG.info("User recognized. Access granted.");
-                chain.doFilter(req,res);
-            }else{
-                LOG.info("User need to sign in. Access denied.");
-                res.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            }
-       }
+    protected void doFilter(HttpServletRequest request, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        Object logged = request.getSession().getAttribute("userName");
+        if (logged == null) {
+            res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
 
+        chain.doFilter(request, res);
     }
 }
