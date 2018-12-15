98651bd7e634b70d411ed133e769c94c41e4c6a4
KrzysztofG90 <Ba552245685>
2018-12-15 19:01:57 +0100

    i8L

--- web-app/src/main/java/com/infoshareacademy/usersengine/services/BandleService.java
+++ web-app/src/main/java/com/infoshareacademy/usersengine/services/BandleService.java
@@ -6,6 +6,6 @@ import java.util.ResourceBundle;
 public class BandleService {
 
     public ResourceBundle getBundle(Locale currentLocale){
-        return ResourceBundle.getBundle("webapp/WEB-INF/properties/MessagesBundle", currentLocale);
+        return ResourceBundle.getBundle("MessagesBundle", currentLocale);
     }
 }
